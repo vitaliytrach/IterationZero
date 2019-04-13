0950dd7b274d7e99994c964fa554ff3e4292edff
vitaliytrach <vitaliytrach@gmail.com>
2019-04-13 15:27:12 -0700

    Started implementing BFS for the NPCs to find a path to take.

--- IterationZero/core/src/com/mygdx/game/systems/NPCMovementSystem.java
+++ IterationZero/core/src/com/mygdx/game/systems/NPCMovementSystem.java
@@ -7,17 +7,21 @@ import com.mygdx.game.components.MapComponent;
 import com.mygdx.game.components.TransformComponent;
 import com.mygdx.game.data.Tile;
 import com.mygdx.game.engine.ComponentManager;
+import com.mygdx.game.engine.WallManager;
 import com.mygdx.game.interfaces.ISystem;
 import com.mygdx.game.utils.DirectionAdapter;
 import com.mygdx.game.utils.EntityIDs;
 
-import java.util.Random;
+import java.util.*;
 
 public class NPCMovementSystem implements ISystem {
+
+    private static final int NPC_MOVEMENT_DISTANCE = 10;
 
     private int id;
     private String type;
     private ComponentManager cm;
+    private WallManager wm;
     private int moveP;
     private Random rand;
     private float moveX, moveY;
@@ -28,6 +32,7 @@ public class NPCMovementSystem implements ISystem {
         type = "NPCMovementSystem";
         moveP = 50;
         cm = ComponentManager.getInstance();
+        wm = WallManager.getInstance();
         rand = new Random();
         direction = 0;
         counter = 0;
@@ -45,10 +50,17 @@ public class NPCMovementSystem implements ISystem {
         return type;
     }
 
+    boolean test = true;
+
     @Override
     public void render() {
         EntityStateComponent esc = (EntityStateComponent) cm.getComponent(id, "EntityStateComponent");
         LocationComponent lc = (LocationComponent) cm.getComponent(id, "LocationComponent");
+
+        if(test) {
+            findNextPath();
+            test = false;
+        }
 
         if(!esc.isMoving()) {
             int roll = rand.nextInt(100);
@@ -103,6 +115,145 @@ public class NPCMovementSystem implements ISystem {
 
             move();
         }
+    }
+
+    private void findNextPath() {
+        LocationComponent lc = (LocationComponent) cm.getComponent(id, "LocationComponent");
+        MapComponent mc = (MapComponent) cm.getComponent(lc.getMap(), "MapComponent");
+        int x = lc.getX();
+        int y = lc.getY();
+
+        int randStartX = x - (NPC_MOVEMENT_DISTANCE / 2) > 0 ? x - (NPC_MOVEMENT_DISTANCE / 2) : 0;
+        int randEndX = x + (NPC_MOVEMENT_DISTANCE / 2) < mc.getWidth() ? x + (NPC_MOVEMENT_DISTANCE / 2) : mc.getWidth() - 1;
+        int randStartY = y - (NPC_MOVEMENT_DISTANCE / 2) > 0 ? y - (NPC_MOVEMENT_DISTANCE / 2) : 0;
+        int randEndY = y + (NPC_MOVEMENT_DISTANCE / 2) < mc.getHeight() ? y + (NPC_MOVEMENT_DISTANCE / 2) : mc.getHeight() - 1;
+
+        int xPos = 0;
+        int yPos = 0;
+
+        while(true) {
+            xPos = rand.nextInt(randEndX) + randStartX;
+            yPos = rand.nextInt(randEndY) + randStartY;
+
+            if(!wm.isWall(lc.getMap(), xPos, yPos)) {
+                break;
+            }
+        }
+
+        String[] path = dfsPath(lc.getMap(), x, y, xPos, yPos);
+
+        for(String s : path) {
+            System.out.println(s);
+        }
+    }
+
+    private String[] dfsPath(int mapId, int startX, int startY, int endX, int endY) {
+        ArrayList<String> path = new ArrayList<String>();
+        HashSet<String> visited = new HashSet<String>();
+        ArrayList<String> queue = new ArrayList<String>();
+
+        int x = startX;
+        int y = startY;
+        int totalX = 0;
+        int totalY = 0;
+
+        String curr = coordToString(x, y);
+        String end = coordToString(endX, endY);
+
+        System.out.println(curr + " looking for: " + end);
+
+        while(true) {
+            path.add(curr);
+
+            // Check if up is visited
+            if(!visited.contains(coordToString(x,y - 1))) {
+                if(!wm.isWall(mapId, x, y - 1) && isValidY(y - 1, totalY)) {
+                    queue.add(coordToString(x, y - 1));
+                    visited.add(coordToString(x, y - 1));
+                    path.add("up");
+                    totalY--;
+                }
+            }
+
+            // Check if down is visited
+            if(!visited.contains(coordToString(x,y + 1))) {
+                if(!wm.isWall(mapId, x, y + 1) && isValidY(y + 1, totalY)) {
+                    queue.add(coordToString(x, y + 1));
+                    visited.add(coordToString(x, y + 1));
+                    path.add("down");
+                    totalY++;
+                }
+            }
+
+            // Check if right is visited
+            if(!visited.contains(coordToString(x + 1, y))) {
+                if(!wm.isWall(mapId, x + 1, y) && isValidX(x + 1, totalX)) {
+                    queue.add(coordToString(x + 1, y));
+                    visited.add(coordToString(x + 1, y));
+                    path.add("right");
+                    totalX++;
+                }
+            }
+
+            // Check if left is visited
+            if(!visited.contains(coordToString(x - 1,y))) {
+                if(!wm.isWall(mapId, x - 1, y) && isValidX(x - 1, totalX)) {
+                    queue.add(coordToString(x - 1, y));
+                    visited.add(coordToString(x - 1, y));
+                    path.add("left");
+                    totalX--;
+                }
+            }
+
+            if(curr.compareTo(end) == 0) {
+                break;
+            }
+
+            visited.add(curr);
+
+            if(queue.isEmpty()) {
+                break;
+            }
+
+            int[] nextCoords = coordToInt(curr);
+            curr = queue.remove(0);
+            x = nextCoords[0];
+            y = nextCoords[1];
+        }
+
+        return path.toArray(new String[0]);
+    }
+
+    private boolean isValidX(int x, int totalX) {
+        if(Math.abs(x) <= NPC_MOVEMENT_DISTANCE / 2) {
+            if(x < ((MapComponent) cm.getComponent(1, "MapComponent")).getWidth() - 1 || x > 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isValidY(int y, int totalY) {
+        if(Math.abs(y) <= NPC_MOVEMENT_DISTANCE / 2) {
+            if(y < ((MapComponent) cm.getComponent(1, "MapComponent")).getHeight() - 1 || y > 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String coordToString(int x, int y) {
+        return Integer.toString(x) + "," + Integer.toString(y);
+    }
+
+    private int[] coordToInt(String coords) {
+        int[] result = new int[2];
+        String[] coord = coords.split(",");
+        result[0] = Integer.parseInt(coord[0]);
+        result[1] = Integer.parseInt(coord[1]);
+        return result;
     }
 
     private void move() {
